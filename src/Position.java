a5914cc4f5021eb024f202e1f11044b58fe786e5
ahyh60 <123133761+ahyh60@users.noreply.github.com>
2023-10-02 18:39:15 +0800

    Create Position.java

---
+++ src/Position.java
@@ -0,0 +1,20 @@
+public class Position {
+    private int row;
+    private int col;
+
+    public Position(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getR(){
+        return row;
+    }
+    public int getC(){
+        return col;
+    }
+
+
+
+
+}
